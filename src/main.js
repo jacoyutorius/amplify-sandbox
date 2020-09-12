7da4992af178e48d64539e233f9610a4e9f37f4d
yutoogi <jacoyutorius@gmail.com>
2020-09-13 04:42:38 +0900

    show config value

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import {
   Analytics
 } from "aws-amplify";
 import awsconfig from "./aws-exports";
+
+console.log("awsconfig", awsconfig);
 Amplify.configure(awsconfig);
 
 // https://qiita.com/akitsukada/items/6dff5a90d81765f83ab6#%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0%E3%83%97%E3%83%AD%E3%83%90%E3%82%A4%E3%83%80%E3%83%BC%E3%81%AE%E4%BE%8B
