2bff9a938dee00552bfb6d09ac8aff6e7fddcf7d
yutoogi <jacoyutorius@gmail.com>
2021-11-02 00:55:57 +0900

    amplify pull

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }} <span style="color:red;">canary</span></h1>
+    <h1>{{ msg }} <span style="color:red;">master</span></h1>
     <p>
       For a guide and recipes on how to configure / customize this project,
       <br />check out the
